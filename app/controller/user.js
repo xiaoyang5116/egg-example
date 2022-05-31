b1799ed5593805847a9187604e014f4819caa806
yx <1443584075@qq.com>
2022-05-31 10:47:48 +0800

    egg-example

---
+++ app/controller/user.js
@@ -0,0 +1,13 @@
+const { Controller } = require("egg");
+
+
+class UserController extends Controller {
+    async info() {
+        const { ctx } = this
+        ctx.body = {
+            name: `hello ${ctx.params.id}`,
+        }
+    }
+}
+
+module.exports = UserController;
